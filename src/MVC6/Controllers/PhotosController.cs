9862d402f77a3efe4bad93356801d675a00addc7
kellerd <danny.keller@tc.gc.ca>
2015-11-22 20:29:29 -0500

    Json Db Results

--- src/MVC6/Controllers/PhotosController.cs
+++ src/MVC6/Controllers/PhotosController.cs
@@ -3,11 +3,13 @@ using Microsoft.AspNet.Mvc;
 using MVC6RC1.Controllers;
 using PagedList;
 using System.Linq;
+using System;
 
 namespace MVC6.Controllers
 {
-    public class PhotosController : Controller
+    public class PhotosController : Controller, IDisposable
     {
+
         private PhotoContext _context;
 
         public PhotosController(PhotoContext context)
@@ -15,11 +17,20 @@ namespace MVC6.Controllers
             _context = context;
         }
 
-        public IActionResult Index(PhotosModel model)
+        public IActionResult Index()
         {
-            var photos = _context.PhotoInfos.OrderByDescending(p => p.date).ToPagedList(model.PageNumber, 10);
-            model.photos = model;
-            return View(model);
+            return View();
+        }
+        [Produces("application/json")]
+        public IActionResult Data(int id = 1)
+        {
+            return Json(_context.PhotoInfos.OrderByDescending(p => p.date).ToPagedList(id, 10));
+        }
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _context.Dispose();
+            _context = null;
         }
     }
 }
